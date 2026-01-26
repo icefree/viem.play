8c8c638296d722f6f1ebf603e5cdaa932b797935
Rex <rex5630@gmail.com>
2026-01-26 18:23:24 +0800

    fix(auto-pair): trigger input now auto-pairs with Timer instead of Button

--- src/nodes/auto-pair.ts
+++ src/nodes/auto-pair.ts
@@ -79,8 +79,8 @@ function attachInputAutoPairing(nodeType: typeof LGraphNode) {
     switch (input.type) {
       case LiteGraph.ACTION:
       case -1: {
-        const buttonNode = createNode(this.graph, NODE_TYPES.button, leftX, y + OFFSET_Y)
-        if (buttonNode) buttonNode.connect(0, this, index)
+        const timerNode = createNode(this.graph, NODE_TYPES.timer, leftX, y + OFFSET_Y)
+        if (timerNode) timerNode.connect(0, this, index)
         break
       }
       case 'string': {
